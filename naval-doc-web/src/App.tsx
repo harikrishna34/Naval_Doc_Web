fedca517908466fa0966ce6b10f9fbfefcfa4da0
New-account-ui <vamulya10@gmail.com>
2025-04-26 14:26:45 +0530

    added userList

--- naval-doc-web/src/App.tsx
+++ naval-doc-web/src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./auth/login";
 import LayoutWrapper from "./components/layout/layoutWrapper";
-import ItemsDashboard from "./modules/items/dashboard";
+import AdminDB from "./modules/admin/adminDB";
+import Billing from "./modules/billing";
+import Cart from "./modules/cart/cart";
+import UserDashboard from "./modules/dashboard";
 import FinanceDB from "./modules/finance/dashboard";
 import InventoryDB from "./modules/inventory/dashboard";
-import UserDashboard from "./modules/dashboard";
-import Login from "./auth/login";
-import Billing from "./modules/billing";
-import Profile from "./modules/profile/profile";
-import Wallet from "./modules/wallet/wallet";
-import Notifications from "./modules/notifications/notifications";
-import Cart from "./modules/cart/cart";
-import AddCanteen from "./modules/superAdmin/addCanteen";
-import AdminDB from "./modules/admin/adminDB";
+import ItemsDashboard from "./modules/items/dashboard";
 import Menu from "./modules/menu/menu";
+import Notifications from "./modules/notifications/notifications";
+import Profile from "./modules/profile/profile";
+import AddCanteen from "./modules/superAdmin/addCanteen";
+import Wallet from "./modules/wallet/wallet";
 
 const App = () => {
   return (
